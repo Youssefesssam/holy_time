bf22447f7ec0a931a93b1fca23c2719174f5666b
G4 <yessam770@gmail.com>
2024-04-25 03:45:53 +0200

    same branch 2

--- lib/ui/screens/features/slider/designCard/designCard.dart
+++ lib/ui/screens/features/slider/designCard/designCard.dart
@@ -1,33 +1,49 @@
 import 'package:flutter/material.dart';
 
 class DesignCard extends StatelessWidget {
-  String titleCard="";
-  Color color =Colors.white;
-   DesignCard({super.key,  required this.titleCard,required this.color});
+  String titleCard = "";
+  Color color = Colors.white;
+  String? path;
+
+
+  DesignCard({
+    super.key,
+    required this.titleCard,
+    required this.color,
+    this.path,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return  Stack(
+    return Stack(
       alignment: Alignment.topCenter,
       children: [
         Opacity(
           opacity: .3,
           child: Card(
-              elevation: 20,
-              child:Container(
-                color: Colors.white ,
-              )
+            elevation: 20,
+            child: Container(
+              color: Colors.white,
+            ),
           ),
         ),
-         Text(
-           titleCard,
-          style:  TextStyle(
-              color: color,
-              fontSize: 20,
-              fontWeight: FontWeight.bold
+        Text(
+          titleCard,
+          style: TextStyle(
+            color: color,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
-          textAlign:TextAlign.center,
-        ) ,
+          textAlign: TextAlign.center,
+        ),
+        if (path != null && path!.isNotEmpty)
+          Center(
+            child: Image.asset(
+              path!,
+              height: MediaQuery.of(context).size.height * .15,
+              width: MediaQuery.of(context).size.width * .15,
+            ),
+          ),
       ],
     );
   }
--- lib/ui/screens/homeScreen/homeScreenUsers.dart
+++ lib/ui/screens/homeScreen/homeScreenUsers.dart
@@ -17,73 +17,72 @@ class HomeScreenUsers extends StatelessWidget {
       body: Container(
         decoration: const BoxDecoration(
             image: DecorationImage(
-          image: AssetImage(AppAssets.backGround),fit: BoxFit.fill
-        )),
+                image: AssetImage(AppAssets.backGround), fit: BoxFit.fill)),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
                 margin: const EdgeInsets.only(
-                    left: 30, right: 30, top: 30, bottom: 20),
-
+                    left: 30, right: 30, top: 30, bottom: 10),
                 child: const AppBarUser()),
-
-      Container(
-          margin: const EdgeInsets.only(left: 30),
-          child: Row(
-            children: [
-              const Text(
-                "Rank#",
-                style: TextStyle(fontSize: 25),
-              ),
-              const Spacer(),
-              Container(
-                margin: const EdgeInsets.only(right: 30),
-                child: const Text(
-                  "I attended",
-                  style: TextStyle(fontSize: 25),
-                ),
-              )
-            ],
-          )),
-      Container(
-        padding: const EdgeInsets.only(left: 30, top: 10,right: 30),
-        child: Row(
-          children: [
-            Card(
-              elevation: 10,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: const Color(0xffffba00),
-                    borderRadius: BorderRadius.circular(10)
-                ),
-                height: 50,
-                width: 90,
-                child: const Center(
-                    child: Text(
-                      '1#',
-                      style: TextStyle(color: Colors.white, fontSize: 25),
-                    )),
+            Container(
+                margin: const EdgeInsets.only(left: 30),
+                child: Row(
+                  children: [
+                    const Text(
+                      "Rank#",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    const Spacer(),
+                    Container(
+                      margin: const EdgeInsets.only(right: 30),
+                      child: const Text(
+                        "I attended",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    )
+                  ],
+                )),
+            Container(
+              padding: const EdgeInsets.only(left: 30, top: 10,right: 30),
+              child: Row(
+                children: [
+                  Card(
+                    elevation: 10,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: const Color(0xffffba00),
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      height: 50,
+                      width: 90,
+                      child: const Center(
+                          child: Text(
+                            '1#',
+                            style: TextStyle(color: Colors.white, fontSize: 25),
+                          )),
+                    ),
+                  ),
+                  const Spacer(),
+                  Card(
+                    elevation: 10,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color(0xeaf84c4c),
+                      ),
+                      height: 50,
+                      width: 100,
+                      child: const Center(
+                          child: Text(
+                            '5%',
+                            style: TextStyle(color: Colors.white, fontSize: 25),
+                          )),
+                    ),
+                  )
+                ],
               ),
             ),
-            const Spacer(),
-            Card(
-              elevation: 10,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: const Color(0xeaf84c4c),
-                ),
-                height: 50,
-                width: 100,
-                child: const Center(
-                    child: Text(
-                      '5%',
-                      style: TextStyle(color: Colors.white, fontSize: 25),
-                    )),
-              ),
-            )
-          ],)),
             Container(
                 margin: const EdgeInsets.only(top: 10, left: 30),
                 child: const Text(
@@ -95,39 +94,47 @@ class HomeScreenUsers extends StatelessWidget {
               height: 10,
             ),
             const Expanded(flex: 5, child: ChartScatter()),
-            Container(
-                margin: const EdgeInsets.only(left: 20, bottom: 20),
-                child: const Text(
-                  "Contents",
-                  style: TextStyle(fontSize: 25),
-                )),
-
+            Row(
+              children: [
+                Container(
+                    margin: const EdgeInsets.only(left: 20, bottom: 20),
+                    child: const Text(
+                      "Contents",
+                      style: TextStyle(fontSize: 25),
+                    )),
+                Container(
+                    margin: const EdgeInsets.only(left: 5, bottom: 20),
+                    child: Image.asset("assets/icons/content.png",height: 30,width: 40,))
+              ],
+            ),
             Container(
               margin: EdgeInsets.all(10),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
               ),
-              height: MediaQuery.of(context).size.height*.13,
+              height: MediaQuery.of(context).size.height * .13,
               child: Row(
                 children: [
                   Expanded(
-                      flex:1,
+                      flex: 1,
                       child: DesignCard(
-                    titleCard: "points",
-                    color: Colors.black,
-                  )),
+                        titleCard: "points",
+                        path:"assets/icons/best-score.png",
+                        color: Colors.black,
+                      )),
                   Expanded(
-                    flex: 1,
-                      child: DesignCard(titleCard: "A Weeekly Word", color: Colors.black)),
+                      flex: 1,
+                      child: DesignCard(
+                          titleCard: "A Weeekly Word", color: Colors.black)),
                   Expanded(
-                    flex: 1,
-                      child: DesignCard(titleCard: "sss", color: Colors.black)),
+                      flex: 1,
+                      child: DesignCard(
+                          path:"assets/icons/best-score.png",
+                          titleCard: "sss", color: Colors.black)),
                 ],
               ),
             ),
-
             SliderPic(),
-
           ],
         ),
       ),
